82ae71f4c4bfcda4c991d7c26c0bdb306cb20a09
aquapaka <aqua.tamlong@gmail.com>
2024-06-11 11:32:42 +0700

    feat: add get tag route, add validate path param

--- nest/src/tags/tags.controller.ts
+++ nest/src/tags/tags.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   Delete,
   ForbiddenException,
+  Get,
   Param,
   Patch,
   Post,
@@ -13,6 +14,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { ClsService } from 'nestjs-cls';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { Action, CaslAbilityFactory } from '../casl/casl-ability.factory';
+import { FindOneParams } from '../validation/findOneParams';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
 import { Tag } from './schemas/tag.schema';
@@ -27,6 +29,12 @@ export class TagsController {
     private readonly cls: ClsService,
   ) {}
 
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  getTag(@Param() params: FindOneParams) {
+    return this.tagsService.findOneTagById(params.id);
+  }
+
   @Post()
   @UseGuards(JwtAuthGuard)
   createTag(@Body() createTagDto: CreateTagDto) {
@@ -36,19 +44,22 @@ export class TagsController {
 
   @Patch(':id')
   @UseGuards(JwtAuthGuard)
-  async updateTag(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
+  async updateTag(
+    @Param() params: FindOneParams,
+    @Body() updateTagDto: UpdateTagDto,
+  ) {
     const user = this.cls.get('user');
     const ability = this.caslAbilityFactory.createForUser(user);
 
     try {
-      const tagToUpdate = await this.tagsService.findOneTagById(id);
+      const tagToUpdate = await this.tagsService.findOneTagById(params.id);
 
       const checkTag = new Tag();
       checkTag._owner = tagToUpdate._owner;
 
       ForbiddenError.from(ability).throwUnlessCan(Action.Update, checkTag);
 
-      return this.tagsService.updateTag(id, updateTagDto);
+      return this.tagsService.updateTag(params.id, updateTagDto);
     } catch (error) {
       if (error instanceof ForbiddenError)
         throw new ForbiddenException(error.message);
@@ -57,19 +68,19 @@ export class TagsController {
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
-  async deleteTag(@Param('id') id: string) {
+  async deleteTag(@Param() params: FindOneParams) {
     const user = this.cls.get('user');
     const ability = this.caslAbilityFactory.createForUser(user);
 
     try {
-      const tagToDelete = await this.tagsService.findOneTagById(id);
+      const tagToDelete = await this.tagsService.findOneTagById(params.id);
 
       const checkTag = new Tag();
       checkTag._owner = tagToDelete._owner;
 
-      ForbiddenError.from(ability).throwUnlessCan(Action.Update, checkTag);
+      ForbiddenError.from(ability).throwUnlessCan(Action.Delete, checkTag);
 
-      return this.tagsService.deleteTag(id);
+      return this.tagsService.deleteTag(params.id);
     } catch (error) {
       if (error instanceof ForbiddenError)
         throw new ForbiddenException(error.message);
