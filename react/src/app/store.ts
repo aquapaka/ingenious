6faa3a4a3ea7b578123fa8b703ccdbe1b236e40d
aquapaka <aqua.tamlong@gmail.com>
2024-06-13 12:46:05 +0700

    refactor: rename check invalid creadential middleware

--- react/src/app/store.ts
+++ react/src/app/store.ts
@@ -1,14 +1,15 @@
 import { mainApi } from '@/services/main-service';
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import { rtkQueryToast } from '../services/middlewares/rtk-query-toast';
+import { rtkQueryCheckInvalidCredential } from '../services/middlewares/rtk-query-check-invalid-credential';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [mainApi.reducerPath]: mainApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(mainApi.middleware).concat(rtkQueryToast),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(mainApi.middleware).concat(rtkQueryCheckInvalidCredential),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
