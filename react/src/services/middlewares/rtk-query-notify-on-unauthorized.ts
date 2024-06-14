90f0d18280940810f818253ab8214819e61eb0fc
aquapaka <aqua.tamlong@gmail.com>
2024-06-14 15:25:12 +0700

    chore: tidy up

--- react/src/services/middlewares/rtk-query-notify-on-unauthorized.ts
+++ react/src/services/middlewares/rtk-query-notify-on-unauthorized.ts
@@ -1,16 +1,17 @@
-import { Middleware, MiddlewareAPI, isRejectedWithValue } from '@reduxjs/toolkit';
+import { Middleware, isRejectedWithValue } from '@reduxjs/toolkit';
 import { toast } from 'sonner';
 
 /**
  * Log a warning and show a toast!
  */
-export const rtkQueryNotifyOnUnAuthorized: Middleware = (api: MiddlewareAPI) => (next) => (action) => {
+export const rtkQueryNotifyOnUnAuthorized: Middleware = () => (next) => (action) => {
   // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
   if (isRejectedWithValue(action)) {
     const payload = action.payload as object;
     if ('status' in payload && payload.status === 403) {
       toast.error('Unauthorized', {
         description: "You don't have access to this resouce",
+        duration: 8000,
       });
     }
   }
